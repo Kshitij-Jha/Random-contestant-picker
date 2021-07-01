67dfefcde96fa2962a88fbb2a4f40f0a8d6e2355
Kshitij Jha <46564016+Kshitij-Jha@users.noreply.github.com>
2021-07-01 08:15:10 +0000

    Added functionality to have unique picks every round

--- main.py
+++ main.py
@@ -6,6 +6,22 @@ from keep_running import keep_alive
 my_secret = os.environ['TOKEN']
 client = discord.Client()
 
+def add(msg):
+    f = open("contestants.txt",'w')
+    f.truncate(0)
+    f.write(msg)
+    arr = msg.split(',')
+    counter = str(len(arr))
+    random.shuffle(arr)
+    separator = ','
+    arr = separator.join(arr)
+    #print(type(arr),arr)
+    f.write("\n")
+    f.write(arr)
+    f.write("\n")
+    f.write(counter+'\n')
+    f.close()
+
 @client.event
 
 async def on_ready():
@@ -19,29 +35,30 @@ async def on_message(message):
     return
 
   if message.content.startswith('!list'):
-    await message.channel.send('**!list:** List all commands, **!test:** Check if bot is online, **!add:** add contestants make sure separated by commas, **!p:** pick random contestant')
+    await message.channel.send('**!list:** List all commands \n**!test:** Check if bot is online \n**!contestants:** Display the list of current contestants  \n**!add:** add contestants make sure separated by commas (need to use this once at the beginning of every round)...until further updates \n**!p:** pick random contestant')
 
   if message.content.startswith('!test'):
     await message.channel.send('Bot online')
 
   if message.content.startswith('!fishie'):
-    await message.channel.send('@Fishie Bear sends :kissing_heart: ')
+    if str(message.author) == 'BipolarBear#6749' or str(message.author) == 'littlefish#4767':
+      await message.channel.send('@Fishie Bear sends :kissing_heart: ')
+
+      with open('tenor.gif', 'rb') as f:
+        picture = discord.File(f)
+        await message.channel.send(file=picture)
 
   if message.content.startswith('!bear'):
     await message.channel.send('@Bear Fishie sends :kissing_heart: ')
-    #await channel.send(file=discord.File('tenor.gif')
 
   if message.content.startswith('!contestants'):
     f = open("contestants.txt",'r')
-    msg = f.read()
+    msg = f.readlines()[0]
     await message.channel.send(msg)
 
   if message.content.startswith('!add'):
-    msg = message.content[4:]
-    f = open("contestants.txt",'w')
-    f.truncate(0)
-    f.write(msg)
-    f.close()
+    msg = message.content[5:]
+    add(msg)
     await message.channel.send('*Contestants added*')
 
   #if message.content.startswith('!addnew'):
@@ -49,10 +66,19 @@ async def on_message(message):
 
   if message.content.startswith('!p'):
     f = open("contestants.txt",'r')
-    msg = f.read()
+    msg = f.readlines()[1]
+    f = open("contestants.txt",'r')
+    counter = int(f.readlines()[-1])
+    counter -= 1
+    f = open("contestants.txt",'a')
+    f.writelines(str(counter) + '\n')
     msg = msg.split(",")
-    choice = random.choice(msg)
-    await message.channel.send(choice)
+    #print(counter)
+    await message.channel.send(msg[counter])
+    if counter == 0:
+      separator = ','
+      msg = separator.join(msg)
+      await message.channel.send("Round is over, Please use command **!add "+ msg + "** (just copy paste that) to start next round")
 
 keep_alive()
 client.run(my_secret)
